4e27a42197817d93b8841bd1c7bfcfe89b1ebab2
Akash G <12345sunnys@gmail.com>
2023-06-10 01:45:50 -0400

    changed import roboto-font.css

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./newlayout/App"
 import reportWebVitals from './reportWebVitals';
+/*
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+*/
+import 'typeface-roboto';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
